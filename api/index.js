aeaa63a8bfffda5c9edb0a87ec9788c37d016d3b
Aaron-Portman <aaronmportman@gmail.com>
2021-12-25 22:54:31 -0600

    fixed build

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get("/home", (req,res) =>{
     res.send({ mileage: 7})
